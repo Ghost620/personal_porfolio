a9fa994b02cedb0e4436199ee30b674527631065
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-05 16:12:37 +0500

    changes for mobile view

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -6,11 +6,11 @@ type Props = {}
 const ContactMe = (props: Props) => {
   return (
     <div className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-2xl'>Contact Me</h3>
+        <h3 className='absolute top-20 md:top-24 uppercase tracking-[20px] text-xl md:text-2xl'>Contact Me</h3>
 
         <div className='flex flex-col space-y-10'>
 
-            <h4 className='text-4xl font-semibold text-center'>
+            <h4 className='md:text-4xl font-semibold text-center mt-16 md:mt-0'>
                 I've got just what you need. {" "}
                 <span className='underline decoration-green-900'>Let's talk</span> 
             </h4>
@@ -18,18 +18,18 @@ const ContactMe = (props: Props) => {
             <div className='space-y-10'>
                 <div className='flex items-center space-x-5 justify-center'>
                     <PhoneIcon className='text-teal-700 h-7 w-7 animate-pulse' />
-                    <p className='text-2xl'> +92 3418183070 </p>
+                    <p className='md:text-2xl'> +92 3418183070 </p>
                 </div>
                 <div>
                     <div className='flex items-center space-x-5 justify-center'>
                         <EnvelopeIcon className='text-teal-700 h-7 w-7 animate-pulse' />
-                        <p className='text-2xl'> faghost6201@gmail.com </p>
+                        <p className='md:text-2xl'> faghost6201@gmail.com </p>
                     </div>
                 </div>
                 <div>
                     <div className='flex items-center space-x-5 justify-center'>
                         <MapPinIcon className='text-teal-700 h-7 w-7 animate-pulse' />
-                        <p className='text-2xl'> Dastagir Society, F.B.Area, Karachi, Pakistan </p>
+                        <p className='md:text-2xl'> Dastagir Society, F.B.Area, Karachi, Pakistan </p>
                     </div>
                 </div>
                 
