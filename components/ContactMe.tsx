295222b3590b5fb820ec1ab05044c9049a488f5a
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-05 20:42:36 +0500

    contact me page completed

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import { useForm, SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+    name: string,
+    email: string,
+    subject: string,
+    message: string
+};
 
 type Props = {}
 
 const ContactMe = (props: Props) => {
+    const { register, handleSubmit} = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (formData) => {
+        window.location.href = `mailto:siddiq4203362@cloud.neduet.edu.pk?subject=${formData.subject}&body=Hi, I'm ${formData.name}, ${formData.message}`;
+    }
+
   return (
     <div className='h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-20 md:top-24 uppercase tracking-[20px] text-xl md:text-2xl'>Contact</h3>
@@ -35,14 +48,14 @@ const ContactMe = (props: Props) => {
                 
             </div>
 
-            <form className='flex flex-col space-y-2 w-fit mx-auto'>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-2 w-fit mx-auto'>
                 <div className='flex space-x-2'>
-                    <input type="text" placeholder='Name' className='contactInput w-40 md:w-auto' /> 
-                    <input type="email" placeholder='Email' className='contactInput w-40 md:w-auto' />
+                    <input {...register('name')} type="text" placeholder='Name' className='contactInput w-40 md:w-auto' /> 
+                    <input {...register('email')} type="email" placeholder='Email' className='contactInput w-40 md:w-auto' />
                 </div>
 
-                <input type="text" placeholder='Subject' className='contactInput' />
-                <textarea placeholder='Message' className='contactInput' />
+                <input {...register('subject')} type="text" placeholder='Subject' className='contactInput' />
+                <textarea {...register('message')} placeholder='Message' className='contactInput' />
                 <button type='submit' className='bg-gradient-to-b from-teal-300 to-teal-800 py-5 hover:from-teal-500 px-10 rounded-md font-bold text-lg text-black'> Submit </button>
             </form>
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { motion } from 'framer-motion';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
 
 type Props = {}
 
 const Header = (props: Props) => {
 
+  const [isDark, setisDark] = useState(true)
+
   const toggleDark = () => {
     document.body.classList.toggle('dark')
+    setisDark(!isDark)
   }
 
   return (
@@ -22,14 +26,8 @@ const Header = (props: Props) => {
       </motion.div>
 
       <motion.div initial={{ x: 500, opacity: 0, scale: 0.5 }} animate={{ x: 0, opacity: 1, scale: 1 }} transition={{ duration:1.5 }} className='flex flex-row items-center text-gray-300 cursor-pointer'>
-        
-        <div onClick={toggleDark}>
-          <button className="" type="button" title="Toggle dark and light mode" aria-label="Switch between dark and light mode (currently light mode)" aria-live="polite">
-            <svg viewBox="0 0 24 24" width="24" height="24" className="mt-2">
-              <path fill="currentColor" d="M12,9c1.65,0,3,1.35,3,3s-1.35,3-3,3s-3-1.35-3-3S10.35,9,12,9 M12,7c-2.76,0-5,2.24-5,5s2.24,5,5,5s5-2.24,5-5 S14.76,7,12,7L12,7z M2,13l2,0c0.55,0,1-0.45,1-1s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S1.45,13,2,13z M20,13l2,0c0.55,0,1-0.45,1-1 s-0.45-1-1-1l-2,0c-0.55,0-1,0.45-1,1S19.45,13,20,13z M11,2v2c0,0.55,0.45,1,1,1s1-0.45,1-1V2c0-0.55-0.45-1-1-1S11,1.45,11,2z M11,20v2c0,0.55,0.45,1,1,1s1-0.45,1-1v-2c0-0.55-0.45-1-1-1C11.45,19,11,19.45,11,20z M5.99,4.58c-0.39-0.39-1.03-0.39-1.41,0 c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0s0.39-1.03,0-1.41L5.99,4.58z M18.36,16.95 c-0.39-0.39-1.03-0.39-1.41,0c-0.39,0.39-0.39,1.03,0,1.41l1.06,1.06c0.39,0.39,1.03,0.39,1.41,0c0.39-0.39,0.39-1.03,0-1.41 L18.36,16.95z M19.42,5.99c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06c-0.39,0.39-0.39,1.03,0,1.41 s1.03,0.39,1.41,0L19.42,5.99z M7.05,18.36c0.39-0.39,0.39-1.03,0-1.41c-0.39-0.39-1.03-0.39-1.41,0l-1.06,1.06 c-0.39,0.39-0.39,1.03,0,1.41s1.03,0.39,1.41,0L7.05,18.36z"></path>
-            </svg>
-          </button>
-        </div>
+
+        { isDark ? <SunIcon onClick={toggleDark} className='h-8' /> : <MoonIcon onClick={toggleDark} className='h-8' /> }
 
         <SocialIcon className='cursor-pointer color-green' network='email' fgColor='silver' bgColor='transparent' href="#contactMe" type='link' />
         <p className='uppercase hidden md:inline-flex text-sm text-black dark:text-white'>
