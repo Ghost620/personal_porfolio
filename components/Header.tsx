5ce0bc868ad48198f6d0e13141fc49d8d28b29c5
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2023-01-15 05:25:05 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,20 +22,22 @@ const Header = ({socials}: Props) => {
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
       <motion.div initial={{ x: -500, opacity: 0, scale: 0.5 }} animate={{ x: 0, opacity: 1, scale: 1 }} transition={{ duration:1.5 }} className='flex flex-row items-center'>
 
-        {socials.map((social) => (
-
+        { socials.map((social) => isDark ? (
           <SocialIcon key={social._id} url={social.url} fgColor='silver' bgColor='transparent' />
-
-        ))}
+        ) : (
+          <SocialIcon key={social._id} url={social.url} fgColor='green' bgColor='transparent' />
+        )
+        ) }
         
       </motion.div>
 
       <motion.div initial={{ x: 500, opacity: 0, scale: 0.5 }} animate={{ x: 0, opacity: 1, scale: 1 }} transition={{ duration:1.5 }} className='flex flex-row items-center text-gray-300 cursor-pointer'>
 
-        { isDark ? <SunIcon onClick={toggleDark} className='h-8' /> : <MoonIcon onClick={toggleDark} className='h-8' /> }
+        { isDark ? <SunIcon onClick={toggleDark} className='h-8' /> : <MoonIcon onClick={toggleDark} className='h-8 text-green-700' /> }
 
         <Link href="#contactMe">
-          <SocialIcon className='cursor-pointer color-green' network='email' fgColor='silver' bgColor='transparent' href="#contactMe" type='link' />
+          { isDark? <SocialIcon className='cursor-pointer color-green' network='email' fgColor='silver' bgColor='transparent' href="#contactMe" type='link' /> : <SocialIcon className='cursor-pointer color-green' network='email' fgColor='green' bgColor='transparent' href="#contactMe" type='link' /> }
+
           <p className='uppercase hidden md:inline-flex text-sm text-black dark:text-white'>
             Get In Touch
           </p>
